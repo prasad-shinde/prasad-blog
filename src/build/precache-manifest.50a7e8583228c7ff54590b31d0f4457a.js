9c2c277f6c41c647d0159163685648c555c5e7e7
Disha Shelke <shelke.disha@gmail.com>
2019-12-26 16:37:33 -0800

    First commit

---
+++ src/build/precache-manifest.50a7e8583228c7ff54590b31d0f4457a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1b5d226636247a75f7d1ca06222c14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b28a0eb964cb4807d10",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "88f775d29ab9c5b3da86",
+    "url": "/static/js/2.cbeb8676.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.cbeb8676.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b28a0eb964cb4807d10",
+    "url": "/static/js/main.54c06b2a.chunk.js"
+  },
+  {
+    "revision": "6e55ea343e46b8691e3d",
+    "url": "/static/js/runtime-main.b5470c2c.js"
+  }
+]);
